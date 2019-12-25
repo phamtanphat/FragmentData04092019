74df0f8e940605071ad6c5179516adceedaa4082
phatdroid94 <phatdroid94@gmail.com>
2019-12-25 20:22:05 +0700

    send data activity to fragment not exists

--- app/src/main/java/com/example/fragmentdata04092019/AndroidFragment.java
+++ app/src/main/java/com/example/fragmentdata04092019/AndroidFragment.java
@@ -3,11 +3,15 @@ package com.example.fragmentdata04092019;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -15,17 +19,25 @@ import android.view.ViewGroup;
  */
 public class AndroidFragment extends Fragment {
 
-
-    public AndroidFragment() {
-        // Required empty public constructor
-    }
-
+    View v;
+    EditText edtAndroid;
+    Button btnAndroid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        v = inflater.inflate(R.layout.fragment_android, container, false);
+        edtAndroid = v.findViewById(R.id.edittextAndroid);
+        btnAndroid = v.findViewById(R.id.buttonAndroid);
+        return v;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Bundle bundle = getArguments();
+        String chuoi = bundle.getString("chuoi");
+        Integer so = bundle.getInt("so");
+        Log.d("BBB", chuoi + so);
+        super.onActivityCreated(savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/fragmentdata04092019/MainActivity.java
+++ app/src/main/java/com/example/fragmentdata04092019/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.fragmentdata04092019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        // 1 : Truyen du lieu cho fragment chua ton tai
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("chuoi","Main send data");
+        bundle.putInt("so",1);
+        androidFragment.setArguments(bundle);
+        fragmentTransaction.add(R.id.frameContainer,androidFragment);
+        fragmentTransaction.commit();
     }
 }
